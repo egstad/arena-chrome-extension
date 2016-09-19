d84f9f826bc3d798cb8d13bb3df61a6214e77332
Jordan Egstad <jordan@egstad.com>
2016-09-19 13:16:08 -0700

    first commit

---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+(function(){var s=document.createElement('script');s.setAttribute('type','text/javascript');s.setAttribute('charset','UTF-8');s.setAttribute('src','https://marklet.are.na/loader.js');document.documentElement.appendChild(s);})()
